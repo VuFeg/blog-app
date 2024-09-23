e282e7e6f1c09de0120db9e1ecb7c26cc291d238
VuFeg <ngduogtruongvu@gmail.com>
2024-09-23 23:51:37 +0700

    update: create api update profile, upload avatar image

--- server/src/routes/media.route.ts
+++ server/src/routes/media.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { uploadImageController } from '~/controllers/media.controller'
+import { uploadAvatarController, uploadImageController } from '~/controllers/media.controller'
 import { accessTokenValidator } from '~/middlewares/auth.middleware'
 import { wrapHandleRequest } from '~/utils/handler'
 
 const mediaRouter = Router()
 
 mediaRouter.post('/upload-image', accessTokenValidator, wrapHandleRequest(uploadImageController))
+mediaRouter.post('/upload-avatar', accessTokenValidator, wrapHandleRequest(uploadAvatarController))
 
 export default mediaRouter
